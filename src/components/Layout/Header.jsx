96e5c400d6dfea8f4f115045b9da5fbc67cc1f99
Camille Linan <camille.linan@hotmail.com>
2023-01-16 00:37:49 +0100

    add icons in header

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,7 +1,14 @@
 import { useContext } from "react";
 import { NavLink } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faPlus, faCircleUser, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import AuthContext from "../../context/authContext";
 import logo from '../../styles/img/hottakes-logo.png'
+
+const homeIcon = <FontAwesomeIcon icon={faHouse} />
+const plusIcon = <FontAwesomeIcon icon={faPlus} />
+const accountIcon = <FontAwesomeIcon icon={faCircleUser} />
+const logIcon = <FontAwesomeIcon icon={faRightFromBracket} />
 
 const Header = () => {
     const authCtx = useContext(AuthContext);
@@ -16,14 +23,16 @@ const Header = () => {
                         title='Accueil'
                         end to='/'
                     >
-                        Accueil
+                        <span className="header_nav_text">Accueil</span>
+                        <i className="header_nav_icon">{homeIcon}</i>
                     </NavLink>
                     <NavLink 
                         className={({ isActive }) => (isActive ? "header_nav_link header_nav_link_1 header_nav_link_active" : "header_nav_link header_nav_link_1 header_nav_link_inactive")}
                         title='Ajouter une sauce'
                         end to='/addSauce'
                     >
-                        Ajouter une sauce
+                        <span className="header_nav_text">Ajouter une sauce</span>
+                        <i className="header_nav_icon">{plusIcon}</i>
                     </NavLink>
                 </nav>
                 <img src={logo} alt='hottakes-logo' className="header_logo" />
@@ -32,7 +41,8 @@ const Header = () => {
                         className={({ isActive }) => (isActive ? "header_nav_link header_nav_link_2 header_nav_link_active" : "header_nav_link header_nav_link_2 header_nav_link_inactive")}
                         title='Mon compte'
                     >
-                        Mon compte
+                        <span className="header_nav_text">Mon compte</span>
+                        <i className="header_nav_icon">{accountIcon}</i>
                     </NavLink>
                     {isLoggedIn ? 
                     <NavLink 
@@ -41,14 +51,16 @@ const Header = () => {
                         onClick={authCtx.logout}
                         end to='/'
                     >
-                        Se déconnecter
+                        <span className="header_nav_text">Se déconnecter</span>
+                        <i className="header_nav_icon">{logIcon}</i>
                     </NavLink> :
                     <NavLink 
                     className={({ isActive }) => (isActive ? "header_nav_link header_nav_link_2 header_nav_link_active" : "header_nav_link header_nav_link_2 header_nav_link_inactive")}
                     title='Se connecter'
                     end to='/Login'
                 >
-                    Se connecter
+                    <span className="header_nav_text">Se connecter</span>
+                    <i className="header_nav_icon">{logIcon}</i>
                 </NavLink> }
                 </nav>
             </div>
